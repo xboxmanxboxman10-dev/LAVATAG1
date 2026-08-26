6e525825a272d752fd29db678e46c4cdc08644f0
dev of gtag copy <xboxmanxboxman10@gmail.com>
2026-08-26 22:21:26 +0000

    anything

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 const settings = {
     TitleId: "8D608",
     SecretKey: "N7XPOQJB8CPZB8U6Q7A1ZNGR1QQE41CAUXJGKC6Q4PIISC69EJ",
-    ApiKey: "28502920776061622",
+    ApiKey: "OC|1296841200171257|",
     Webhook: "ugyhsadjhkgbasda"
 };
 
