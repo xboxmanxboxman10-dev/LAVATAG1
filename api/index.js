138c42d42803a4f7b3877bf2e3a1ba290c3d0937
dev of gtag copy <xboxmanxboxman10@gmail.com>
2026-08-28 21:30:47 +0000

    anything git commit -m anything

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 const settings = {
     TitleId: "8D608",
     SecretKey: "N7XPOQJB8CPZB8U6Q7A1ZNGR1QQE41CAUXJGKC6Q4PIISC69EJ",
-    ApiKey: "OC|1296841200171257|afac58dab345e294f3339925c9d11277",
+    ApiKey: "OC|1293481997173844|1ce33959b78ca53fc3d9970ddefc4d5c",
     Webhook: ""
 };
 
